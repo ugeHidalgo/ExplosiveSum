883b992810dba318e24a194243bfec4a805bf1ee
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-12-16 20:43:02 +0100

    Added function to remove duplicated sums

--- spec/KataSpec.js
+++ spec/KataSpec.js
@@ -6,6 +6,26 @@ describe('Kata', function() {
       expect(true).toBeTruthy();
     });
 
+    describe ('removeDuplicateds',function () {
+
+        it('should reorder the elements in the array', function() {
+          var sums = ['31','231','4213'], result = [];
+              
+          result = removeDuplicateds(sums);
+          expect(result[0]).toEqual('13','31 should be 13');
+          expect(result[1]).toEqual('123','231 should be 123');
+          expect(result[2]).toEqual('1234','4213 should be 1234');
+        }); 
+
+        it('should remove duplicated elements in the array', function() {
+          var sums = ['123','231','321'], result = [];
+              
+          result = removeDuplicateds(sums);
+          expect(result[0]).toEqual('123','First element should be 123.');
+          expect(result.length).toEqual( 1,'Should have one element only.');
+        });
+    });
+
     describe ('Trivial cases',function () {
 
         it('should calculate explosive sum of -1', function() {
@@ -38,7 +58,7 @@ describe('Kata', function() {
 
         it('should calculate explosive sum of 3', function() {
           var num = 3,
-              expected = 5;
+              expected = 3;
 
           expect(sum(num)).toEqual(expected,'sum(' + num + ') should be ' + expected);
         });
--- src/Kata.js
+++ src/Kata.js
@@ -1,18 +1,41 @@
 function sum(num){
-  var sums =[];
+  var sums =[], finalSums = [];
 
   sums = calculateSum(num);
-  return sums.length;
+  finalSums = removeDuplicateds(sums);
+  console.log (num + ':' + finalSums);
+  return finalSums.length;
 }
 
 function calculateSum(num){
-  var sums = [];
+  var sums = [], auxSums = [];
 
   if (num <= 0) return sums;
   if (num === 1) return ['1'];
+  sums.push(num.toString());
   for (var f=num; f>0; f--){
-    sums.push(f.toString()+calculateSum(num-f));
-  }
-    
+    auxSums = calculateSum(num-f);
+    auxSums.forEach(function(auxSum){
+      sums.push(f.toString()+auxSum);
+    });
+  } 
   return sums;
 }
+
+function removeDuplicateds(sums){
+  var result = [], 
+      temp = [],
+      aux;
+  sums.forEach(function(sum){
+    aux = sum.split('').sort().join('');
+    temp.push(aux);
+  });
+  for (var f=0; f<temp.length; f++)
+  {
+    if (temp[f]!==temp[f+1]){
+      result.push(temp[f]);
+    }
+  }
+
+  return result;
+};
